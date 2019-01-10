cfdd5e8947eba6abcafaf0213e3f542391c6dab5
sipyrkh <38316912+sipyrkh@users.noreply.github.com>
2019-01-11 01:02:25 +0300

    Update MainTask_01.java

--- src/by/epam/javatraining/ZayatsArtyom/tasks/maintask01/controller/MainTask_01.java
+++ src/by/epam/javatraining/ZayatsArtyom/tasks/maintask01/controller/MainTask_01.java
@@ -9,79 +9,71 @@ import java.util.Arrays;
 public class MainTask_01 {
     static Logger log = Logger.getLogger(MainTask_01.class);
     public static void main(String[] args) {
-        log.info("Программа успешно запустилась");
-        Printer.print("Введите размерность вектора N:");
+        log.info("Program has been started");
+        Printer.print("Input size of vector:");
         int n = UserInput.inputDataInt();
-        int myVector[] = Vector.generateVector(n);
-        Printer.print("Массив:" + Arrays.toString(myVector));
-        log.info("Сгенерированный массив " + Arrays.toString(myVector));
+        int myVector[] = GenerateUserVector.generateVector(n);
+        Printer.print("Your vector: " + Arrays.toString(myVector));
+        log.info("Generated vector: " + Arrays.toString(myVector));
 
-        Printer.printMaxMinValues("Минимальный элемент массива: ", ExtremeValues.searchExtremeMin(myVector));
-        Printer.printMaxMinValues("Максимальный элемент массива: ", ExtremeValues.searchExtremeMax(myVector));
-        log.info("Минимальный элемент массива: " + ExtremeValues.searchExtremeMin(myVector));
-        log.info("Максимальный элемент массива: " + ExtremeValues.searchExtremeMax(myVector));
+        Printer.print("Min value: " + ExtremeValues.searchExtremeMin(myVector));
+        Printer.print("Max value: " + ExtremeValues.searchExtremeMax(myVector));
+        log.info("Min value: " + ExtremeValues.searchExtremeMin(myVector));
+        log.info("Max value: " + ExtremeValues.searchExtremeMax(myVector));
 
-        Printer.printArithmeticGeometricValues("Среднеарифметическое значение всех элементов: ",
-                MeanValues.arithmeticMean(myVector));
-        Printer.printArithmeticGeometricValues("Среднегеометрическое значение всех элементов: ",
-                MeanValues.geometricMean(myVector));
-        log.info("Среднеарифметическое значение всех элементов: " +
-                MeanValues.arithmeticMean(myVector));
-        log.info("Среднегеометрическое значение всех элементов: " +
-                MeanValues.geometricMean(myVector));
+        Printer.print("Arithmetic mean: " + MeanValues.arithmeticMean(myVector));
+        Printer.print("Geometric mean: " + MeanValues.geometricMean(myVector));
+        log.info("Arithmetic mean: " + MeanValues.arithmeticMean(myVector));
+        log.info("Geometric mean: " + MeanValues.geometricMean(myVector));
 
         if(SortChecking.isSortedVectorASC(myVector) | SortChecking.isSortedVectorDESC(myVector)){
-            Printer.print("Массив отсортирован");
-            log.info("Массив отсортирован");
+            Printer.print("Vector is sorted");
+            log.info("Vector is sorted");
         }else {
-            Printer.print("Массив не отсортирован");
-            log.info("Массив не отсортирован");
+            Printer.print("Vector not sorted");
+            log.info("Vector not sorted");
         }
         int localMin = LocalValues.findLocalMin(myVector);
         int localMax = LocalValues.findLocalMax(myVector);
         if(localMin != -1){
-            Printer.printLocalMinAndMAx("Найден индекс первого локального минимума: ", localMin);
-            log.info("Найден индекс первого локального минимума: " + localMin);
+            Printer.print("Index of first local minimum: " + localMin);
+            log.info("Index of first local minimum: " + localMin);
         }
         else if(localMax != -1) {
-            Printer.printLocalMinAndMAx("Найден индекс первого локального максимума: ", localMax);
-            log.info("Найден индекс первого локального максимума: " + localMax);
+            Printer.print("Index of first local maximum: " + localMax);
+            log.info("Index of first local maximum: " + localMax);
         }else{
-            Printer.print("Локальный минимум(максимум) не был найден");
-            log.info("Локальный минимум(максимум) не был найден");
+            Printer.print("Local minimum (maximum) not founded");
+            log.info("Local minimum (maximum) not founded");
         }
-        Printer.print("Введите число для поиска его в массиве: ");
+        Printer.print("Input value to searching: ");
         int searchElement = UserInput.inputDataInt();
-        log.info("Было введено число " + searchElement + " для поиска в массиве");
-        int indexSerchedElementLinear = SearchElements.searchElementLinear(myVector, searchElement);
-        int indexSerchedElementBinary = SearchElements.searchBinary(myVector, 0, myVector.length-1, searchElement);
-        if(indexSerchedElementLinear != 0){
-            Printer.printLocalMinAndMAx("(Линейный поиск) Найденный элемент находится на позиции: ",
-                    indexSerchedElementLinear);
-            log.info("(Линейный поиск) Найденный элемент находится на позиции: " +
-                    indexSerchedElementLinear);
+        log.info("Entered number " + searchElement + " to searching in vector");
+        int indexSearchedElementLinear = SearchElements.searchElementLinear(myVector, searchElement);
+        int indexSearchedElementBinary = SearchElements.searchBinary(myVector, 0, myVector.length-1, searchElement);
+        if(indexSearchedElementLinear != 0){
+            Printer.print("(Linear searching) Index of founded element: " + indexSearchedElementLinear);
+            log.info("(Linear searching) Index of founded element: " + indexSearchedElementLinear);
         }else{
-            Printer.print("Не было найдено элементов");
-            log.info("Не было найдено элементов");
+            Printer.print("Element not found");
+            log.info("Element not found");
         }
-        if(indexSerchedElementBinary != 0){
-            Printer.printLocalMinAndMAx("(Бинарный поиск) Найденный элемент находится на позиции: ",
-                    indexSerchedElementBinary);
-            log.info("(Бинарный поиск) Найденный элемент находится на позиции: " +
-                    indexSerchedElementBinary);
+        if(indexSearchedElementBinary != 0){
+            Printer.print("(Binary searching) Index of founded element: " + indexSearchedElementBinary);
+            log.info("(Binary searching) Index of founded element: " + indexSearchedElementBinary);
         }else{
-            Printer.print("Не было найдено элементов");
-            log.info("Не было найдено элементов");
+            Printer.print("Element not found");
+            log.info("Element not found");
         }
-        Printer.print("Массив с реверсированными элементами: " + Arrays.toString(ReversingVector.reverseElements(myVector)));
-        log.info("Массив с реверсированными элементами: " + Arrays.toString(ReversingVector.reverseElements(myVector)));
-        Printer.print("Сортировка пузырьком: " + Arrays.toString(SortVector.bubbleSort(myVector)));
-        log.info("Сортировка пузырьком: " + Arrays.toString(SortVector.bubbleSort(myVector)));
-        Printer.print("Сортировка вставками: " + Arrays.toString(SortVector.insertionSort(myVector)));
-        log.info("Сортировка вставками: " + Arrays.toString(SortVector.insertionSort(myVector)));
+        Printer.print("Vector with reversing elements: " + Arrays.toString(ReversingVector.reverseElements(myVector)));
+        log.info("Vector with reversing elements: " + Arrays.toString(ReversingVector.reverseElements(myVector)));
+        Printer.print("Bubble sort: " + Arrays.toString(SortVector.bubbleSort(myVector)));
+        log.info("Bubble sort: " + Arrays.toString(SortVector.bubbleSort(myVector)));
+        Printer.print("Insertion sort: " + Arrays.toString(SortVector.insertionSort(myVector)));
+        log.info("Insertion sort: " + Arrays.toString(SortVector.insertionSort(myVector)));
         SortVector.quickSort(myVector, 0, myVector.length-1);
-        Printer.print("Сортировка быстрая: " + Arrays.toString(myVector));
-        log.info("Сортировка быстрая: " + Arrays.toString(myVector));
-        log.info("Программа успешно завершена!");
+        Printer.print("Quick sort: " + Arrays.toString(myVector));
+        log.info("Quick sort: " + Arrays.toString(myVector));
+        log.info("Program has been finished!");
     }
 }
