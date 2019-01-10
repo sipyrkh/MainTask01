f80e54c3625490393a897cac5e8dac69d33e9192
sipyrkh <38316912+sipyrkh@users.noreply.github.com>
2019-01-11 01:04:22 +0300

    Add files via upload

---
+++ src/by/epam/javatraining/ZayatsArtyom/tasks/maintask01/model/GenerateUserVector.java
@@ -0,0 +1,14 @@
+package by.epam.javatraining.ZayatsArtyom.tasks.maintask01.model;
+
+import java.util.Random;
+
+public class GenerateUserVector {
+    public static int[] generateVector(int n){
+        Random rand = new Random();
+        int myVector[] = new int[n];
+        for(int i = 0; i < myVector.length; i++){
+            myVector[i] = rand.nextInt(10);
+        }
+        return myVector;
+    }
+}
